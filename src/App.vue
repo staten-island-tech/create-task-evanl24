3929c696b820878cd1b5ae92c4ef3391a312c33a
snacccc <evanl24@nycstudents.net>
2023-02-16 13:14:58 -0500

    vue setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+
+
+
+</template>
+
+<script>
+
+  import axios from 'axios'
+  import 'Button' from '../components/Button.vue'
+
+
+</script>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+
+import '../main.css'
+
+createApp(App).mount('#app')
