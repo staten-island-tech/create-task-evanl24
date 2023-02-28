ffe38b8c21f9df89df102220c38dae7e233efbdc
snacccc <evanl24@nycstudents.net>
2023-02-28 13:08:01 -0500

    options list is working

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-let objectArrayKey = 'https://rps101.pythonanywhere.com/api/v1/objects/all'
+let key1 = 'https://rps101.pythonanywhere.com/api/v1/objects/all'
 
 const choiceList = document.getElementById('choiceList');
 
 
 
-async function fetchData (x) {
-  const response = await fetch(x)
-  console.log(response)
-  const data = await response.data
+async function fetchData (key) {
+  const response = await fetch(key)
+  const data = await response.json();
   console.log(data)
 
+  data.forEach(element => {
+    let newElement = document.createElement('option');
+    newElement.setAttribute('value', `${element}`)
+    let newElementText = document.createTextNode(`${element}`)
+
+    newElement.appendChild(newElementText)
+    choiceList.appendChild(newElement)
+  });
 }
 
-fetchData(objectArrayKey);
+fetchData(key1);
+
+const randomNum = function () {
+  return Math.floor(Math.random() * 100);
+}
+
